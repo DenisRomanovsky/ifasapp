45af097aa8a8f55e0c3565701ee8607716a0be8f
Denis Romanovsky <dromanovsky@elinext.com>
2016-10-04 15:08:45 +0300

    Change the order of angular-based libs loading.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require angular-rails-templates
 //= require angular/angular
+//= require angular-rails-templates
 //= require angular-route/angular-route
 //= require_tree .
