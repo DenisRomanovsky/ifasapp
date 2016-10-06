6aafd4653db591a16fd49dd3755170830eede970
Denis Romanovsky <dromanovsky@elinext.com>
2016-10-06 22:38:50 +0300

    Init.

--- app/assets/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require angular/angular
-//= require angular-rails-templates
-//= require angular-route/angular-route
-//= require angular-resource/angular-resource
-//= require_tree .
