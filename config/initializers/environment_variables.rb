9cf7684322b48dbf5a16beb84222d1b732e52eb8
Denis Romanovsky <dromanovsky@elinext.com>
2016-10-10 17:05:43 +0300

    Moved load of env for production. Fix. Again.

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -1,10 +1,9 @@
 module EnvironmentVariablesExample
   class Application < Rails::Application
     config.before_configuration do
-      return if Rails.env.production?
       env_file = Rails.root.join("config", 'environment_variables.yml').to_s
 
-      if File.exists?(env_file)
+      if File.exists?(env_file) && !Rails.env.production?
         YAML.load_file(env_file)[Rails.env].each do |key, value|
           ENV[key.to_s] = value
         end # end YAML.load_file
