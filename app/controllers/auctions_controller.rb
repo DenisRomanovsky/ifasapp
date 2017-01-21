bc11069c869e45569e0574e039c7f315fe311e6f
Denis Romanovsky <dromanovsky@elinext.com>
2017-01-21 16:35:00 +0300

    Fix the auction form. Scheduler change.

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -5,7 +5,14 @@ class AuctionsController < ApplicationController
 
   def new
     @auction = Auction.new
-    @auction_category_id = MechanismSubcategory.find(params['mech_subcategory']).mechanism_category_id if params['mech_subcategory']
+    auction_sub_category = MechanismSubcategory.find(params['mech_subcategory']) if params['mech_subcategory']
+
+    if auction_sub_category.nil?
+      @auction_category_id = MechanismCategory.first.id
+    else
+      @auction_category_id = auction_sub_category.mechanism_category_id
+      @auction_sub_category_id = auction_sub_category.id
+    end
   end
 
   def create
@@ -13,6 +20,8 @@ class AuctionsController < ApplicationController
     time_now = Time.now.utc + 5.seconds
     auction_parameters.merge!({user_id: current_user.id, start_time: time_now, status: :active})
     @auction = Auction.new(auction_parameters)
+
+    @auction.end_time = @auction.end_time - 3.hours # Make sure we save time in Minsk Time.
 
     if @auction.save
       redirect_to auctions_path, flash: { notice: 'Аукцион создан.' }
@@ -30,6 +39,11 @@ class AuctionsController < ApplicationController
 
   def edit
     @auction = Auction.where(user_id: current_user.id, id: params[:id].to_i).first
+    @auction.end_time = @auction.end_time + 3.hours
+    @auction_category_id = @auction.mechanism_category_id
+    @auction_sub_category_id = @auction.mechanism_subcategory_id
+    @edit_action = true
+
     raise ActionController::RoutingError.new('Страница не найдена') unless @auction.present?
   end
 
