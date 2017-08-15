8a3994ebaa8d91341115a7a700c44ac6ee249008
dromanovsky <d.romanovskybox@gmail.com>
2017-08-15 10:27:38 -0700

    Redis fix.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,8 @@
 if Rails.env == "production"
   uri = URI.parse(Rails.application.config.x.redis_url)
-  #puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-  #puts Rails.application.config.x.redis_url
-  #puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+  puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+  puts Rails.application.config.x.redis_url
+  puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
   Resque.redis = Redis.new host:uri.host, port:uri.port, password:uri.password
 else
 
