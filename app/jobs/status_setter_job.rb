b15a07223093de9904240c2f07688d505b0760d8
dromanovsky <DenisRamanouski1@epam.com>
2017-08-16 15:49:38 +0300

    Resque fix attempt.

--- app/jobs/status_setter_job.rb
+++ app/jobs/status_setter_job.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class StatusSetterJob < ActiveJob::Base
-  queue_as :default
+  queue_as :status_setter_queue
 
   def perform
-    Rails.logger.fatal '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    Rails.logger.fatal 'JOB STARTED ' + Time.now.to_s
-    Rails.logger.fatal '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    Auction.includes(:owner).active.where('end_time < ?', Time.now.utc).each do |auction|
+    Rails.logger.fatal '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!        STATUS SETTER JOB STARTED ' + Time.now.to_s
+    auctions =  Auction.includes(:owner).active.where('end_time < ?', Time.now.utc)
+    Rails.logger.fatal '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!        STATUS SETTER AUCTIONS SIZE ' + auctions.size.to_s
+
+    auctions.each do |auction|
       auction.update_attribute(:status, :finished)
 
       UserMailer.auction_finished_owner_email(auction.owner.email || auction.email, auction.id).deliver_later
