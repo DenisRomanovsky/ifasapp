228e5fa54e8bbcad01945a252da2ec1661baa26c
dromanovsky <d.romanovskybox@gmail.com>
2017-08-18 08:08:47 -0700

    Loggers for jobs.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,10 @@
 class ApplicationJob < ActiveJob::Base
   before_perform do |job|
     ActiveRecord::Base.clear_active_connections!
+    Rails.logger.fatal("Job starts: #{job}")
+  end
+
+  after_perform do |job|
+    Rails.logger.fatal("Job ended: #{job}")
   end
 end
