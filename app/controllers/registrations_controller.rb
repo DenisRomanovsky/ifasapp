b6344ad669acf11ea158ca77ef1880f08f099188
dromanovsky <d.romanovskybox@gmail.com>
2017-08-18 10:20:54 -0700

    Fix redirection 5.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_inactive_sign_up_path_for(resource)
+  def after_confirmation_path_for(resource)
     edit_profile_path
   end
 end
