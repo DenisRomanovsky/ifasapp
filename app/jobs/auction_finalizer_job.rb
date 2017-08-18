bba3cdfe1f38a457f889a63830b3d21360ba3e77
dromanovsky <d.romanovskybox@gmail.com>
2017-08-18 09:06:39 -0700

    Some more logging.

--- app/jobs/auction_finalizer_job.rb
+++ app/jobs/auction_finalizer_job.rb
@@ -5,22 +5,25 @@ class AuctionFinalizerJob < ApplicationJob
   queue_as :auction_finalizers_queue
 
   def perform(auction_id)
+    Rails.logger.fatal("STATUS UPDATE JOB: #{auction_id}")
+    puts("STATUS UPDATE JOB: #{auction_id}")
+
     auction = Auction.includes(:owner).find(auction_id)
     auction.finished!
 
-    sent_owner_email(auction)
-    sent_bidder_emails(auction)
+    send_owner_email(auction)
+    send_bidder_emails(auction)
   end
 
   private
 
-  def sent_bidder_emails(auction)
+  def send_bidder_emails(auction)
     auction.bidders.each do |bidder|
       UserMailer.auction_finished_bidder_email(bidder.id, auction.id).deliver_later
     end
   end
 
-  def sent_owner_email(auction)
+  def send_owner_email(auction)
     owner_email = auction.owner.try(:email) || auction.user_email
     UserMailer.auction_finished_owner_email(owner_email, auction.id).deliver_later if owner_email.present?
   end
