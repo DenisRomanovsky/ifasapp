28ba04ff06f10c7230e32929579d3902b6b1ca9c
dromanovsky <d.romanovskybox@gmail.com>
2017-08-20 12:07:56 -0700

    Redis for admin.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This redis is needed for the admin pages.
 if Rails.application.config.x.redis_url
-  #$redis = Redis.new(url: Rails.application.config.x.redis_url)
+  $redis = Redis.new(url: Rails.application.config.x.redis_url)
 end
