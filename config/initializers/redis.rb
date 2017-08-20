f09c394e762e37add48e410da3b54f2cc1dc7002
dromanovsky <d.romanovskybox@gmail.com>
2017-08-20 12:19:34 -0700

    Redis for admin.3

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,5 @@
 
 # This redis is needed for the admin pages.
 if Rails.application.config.x.redis_url
-  #$redis = Redis.new(url: Rails.application.config.x.redis_url)
+  $redis = Redis.new(url: Rails.application.config.x.redis_url)
 end
