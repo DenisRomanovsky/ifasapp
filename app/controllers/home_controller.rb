f9707c4308e2525ca86b9a4d3a44126f82728bb5
dromanovsky <DenisRamanouski1@epam.com>
2017-08-24 11:40:44 +0300

    Fix home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   # before_action :authenticate_user!
 
   def index
-    unless current_user.has_info?
+    if current_user && !current_user.has_info?
       flash[:notice] = 'Заполните все поля, пожалуйста.'
       redirect_to edit_profile_path
     end
